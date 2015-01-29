5f7f057d935966b7118720ddd7351d9c340a00c0
walkerkd <walkerkd@dhcp-v80-100183.wireless.unc.edu>
2015-01-29 08:37:03 -0500

    adding files from class

---
+++ script.js
@@ -0,0 +1,18 @@
+//this file is used for class.
+var todayDate= new Date();
+
+
+function myFunction(){
+    alert("testing")
+}
+
+document.getElementById("hello").innerHTML="Hello, Kristi";
+document.getElementById("todayDate").innerHTML= todayDate;
+
+
+
+var myNumberVar=5;
+var myTextVar="hello";
+
+console.log(myNumberVar);
+console.log(myTextVar);
